a69de15d523a5c776b6501a3462de3dc8a4be555
simonlary <simonlary@gmail.com>
2021-07-09 19:31:45 -0400

    Remove Google Translate's text-to-speech api since it no longer works and use the built-in Discord tts instead.

--- src/tts.ts
+++ src/tts.ts
@@ -1,59 +1,14 @@
 import { Message } from "discord.js";
 import { Bot } from "./bot";
 import { Config } from "./config";
-import * as http from "http";
 import * as lorembarnak from "lorembarnak";
 
-const MAX_TTS_LENGTH = 100;
-const TTS_LANG = "en";
 const NUMBER_OF_SWEARS = 10;
 
 export class TTS {
 	constructor(config: Config, bot: Bot) {
-		bot.registerCommand("tts", this.tts, "Text to speech, easy.");
 		bot.registerCommand("lorembarnak", this.lorembarnak, "Highlight the rich cultural landscape of Québécois swears by providing random chains of obscenities.");
 		bot.registerCommand("lorembarnaktts", this.lorembarnaktts, "Highlight the rich cultural landscape of Québécois swears by providing random chains of obscenities told right in your ears.");
-	}
-
-	private tts = async (msg: Message, args: string[]) => {
-		if (msg.member == null) { return msg.channel.send("Message sent by no one."); }
-		if (msg.client.user == null) { return msg.channel.send("The bot is not any user."); }
-
-		// Voice channel checks
-		const voiceChannel = msg.member.voice.channel;
-		if (!voiceChannel)
-			return msg.channel.send("You need to be in a voice channel to play audios!");
-		const permissions = voiceChannel.permissionsFor(msg.client.user);
-		if (permissions != null && !permissions.has("CONNECT"))
-			return msg.channel.send("I cannot connect to your voice channel, make sure I have the proper permissions!.");
-		if (permissions != null && !permissions.has("SPEAK"))
-			return msg.channel.send("I cannot speak in this voice channel, make sure I have the proper permissions!.");
-		if (msg.guild != null && msg.guild.voice != null && msg.guild.voice.connection)
-			return msg.channel.send("I am already in use in a voice channel!");
-
-		// Message length check
-		const message = args.join(" ");
-		if (message.length > MAX_TTS_LENGTH)
-			return msg.channel.send("This message is too long for TTS.");
-
-		try {
-			await voiceChannel.join();
-			const url = this.createUrl(message, TTS_LANG);
-			http.get(url, res => {
-				if (msg.guild?.voice?.connection != null)
-					msg.guild.voice.connection.play(res)
-						.on("finish", () => {
-							if (msg.guild?.me?.voice.channel != null)
-								msg.guild.me.voice.channel.leave();
-						})
-						.on("error", (error) => {
-							console.error(`Error playing tts : ${error}`);
-						});
-			});
-		} catch (error) {
-			console.error(`Could not join voice channel : ${error}`);
-			return msg.channel.send("I could not join the voice channel");
-		}
 	}
 
 	private lorembarnak = async (msg: Message) => {
@@ -61,44 +16,6 @@ export class TTS {
 	}
 
 	private lorembarnaktts = async (msg: Message) => {
-		if (msg.member == null) { return msg.channel.send("Message sent by no one."); }
-		if (msg.client.user == null) { return msg.channel.send("The bot is not any user."); }
-
-		// Voice channel checks
-		const voiceChannel = msg.member.voice.channel;
-		if (!voiceChannel)
-			return msg.channel.send("You need to be in a voice channel to play audios!");
-		const permissions = voiceChannel.permissionsFor(msg.client.user);
-		if (permissions != null && !permissions.has("CONNECT"))
-			return msg.channel.send("I cannot connect to your voice channel, make sure I have the proper permissions!.");
-		if (permissions != null && !permissions.has("SPEAK"))
-			return msg.channel.send("I cannot speak in this voice channel, make sure I have the proper permissions!.");
-		if (msg.guild != null && msg.guild.voice != null && msg.guild.voice.connection)
-			return msg.channel.send("I am already in use in a voice channel!");
-
-		const message = lorembarnak.getText(NUMBER_OF_SWEARS);
-		try {
-			await voiceChannel.join();
-			const url = this.createUrl(message, "fr-CA");
-			http.get(url, res => {
-				if (msg.guild?.voice?.connection != null)
-					msg.guild.voice.connection.play(res)
-						.on("finish", () => {
-							if (msg.guild?.me?.voice.channel != null)
-								msg.guild.me.voice.channel.leave();
-						})
-						.on("error", (error) => {
-							console.error(`Error playing tts : ${error}`);
-						});
-			});
-		} catch (error) {
-			console.error(`Could not join voice channel : ${error}`);
-			return msg.channel.send("I could not join the voice channel");
-		}
-	}
-
-	private createUrl(message: string, lang: string): string {
-		const encoded = encodeURIComponent(message);
-		return `http://translate.google.com/translate_tts?ie=UTF-8&tl=${lang}&q=${encoded}&client=tw-ob&textlen=${message.length}`;
+		msg.channel.send(lorembarnak.getText(NUMBER_OF_SWEARS), { tts: true });
 	}
 }
