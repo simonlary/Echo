5c975776b9f7417f3908017aa23efd5cebd01d96
simonlary <simonlary@gmail.com>
2024-07-31 21:15:35 -0400

    Update dependencies.

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    root: true,
-    env: { node: true, es2021: true },
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-    parser: "@typescript-eslint/parser",
-    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-    ignorePatterns: ["out/**"],
-};
